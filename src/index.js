27cca04f574f99a8997e99b78e227c1d9ed7134b
hsskey <slingshot800@gmail.com>
2018-08-07 12:20:58 +0900

    video 컴포넌트 추가 및 array map 추가

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+//Create a new component. This component should produce 
+//some HTML
 
-import App from './components/app';
-import reducers from './reducers';
+//ES2016 이전에는 변수할당은 var였으나 이후 상수는 const(최종값이 변하지않음&재할당x)
+const API_KEY = 'AIzaSyDupkj08mxyVgCyvSAeKsFSiTRO9aECkwY';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {videos : []}
+    YTSearch({key:API_KEY,term:'surfboard'},(videos) =>{
+      
+      //this.setState({videos : videos})과 같다
+      this.setState({videos})
+    })
+  }
+  
+  
+  render(){
+    
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />,document.querySelector('.container'));
+//Take this component's generated HTML and put it
+//on the page (in the dom)
