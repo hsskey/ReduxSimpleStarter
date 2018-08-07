0f6a9340b7ae73f17acfc8b2a03c7d0663221d69
hsskey <slingshot800@gmail.com>
2018-08-07 15:41:33 +0900

    검색바 props 기능 추가

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,10 @@ import VideoListItem from './video_list_item'
 const VideoList = (props) =>{
     const videoItems = props.videos.map((video)=>{
         return (
-            <VideoListItem video={video} key ={video.etag}/>
+            <VideoListItem 
+            onVideoSelect = {props.onVideoSelect}
+            video={video} 
+            key ={video.etag}/>
         );
     });
 
--- src/index.js
+++ src/index.js
@@ -16,21 +16,32 @@ class App extends Component {
     super(props);
     
     this.state = {videos : []}
-    YTSearch({key:API_KEY,term:'captain marvel'},(videos) =>{
+    this.videoSearch('captain marvel')
+    
+  }
+  videoSearch(term){
+    YTSearch({key:API_KEY,term:term},(videos) =>{
       
       //this.setState({videos : videos})과 같다
-      this.setState({videos})
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      })
     })
   }
-  
   
   render(){
     
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term =>this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList  
+          onVideoSelect={selectedVideo =>this.setState({
+            selectedVideo
+          })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
